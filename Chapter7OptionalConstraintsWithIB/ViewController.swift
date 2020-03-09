bee70f16c27994a9947b3f75a825456f98da22e4
Herve Desrosiers <hdesrosiers@stingray.com>
2020-03-09 10:21:29 -0400

    Initial Commit

---
+++ Chapter7OptionalConstraintsWithIB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chapter7OptionalConstraintsWithIB
+//
+//  Created by Herve Desrosiers on 2020-03-09.
+//  Copyright © 2020 Herve Desrosiers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
